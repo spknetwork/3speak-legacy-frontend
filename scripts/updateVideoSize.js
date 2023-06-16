f932250fb055b19b32e3abdf90b78376a772928b
Sagar <sagar.devices@gmail.com>
2023-06-16 17:24:17 +0530

    not queue at the moment

--- scripts/updateVideoSize.js
+++ scripts/updateVideoSize.js
@@ -1,21 +1,56 @@
 require("../page_conf");
+const { default: PQueue } = require("p-queue");
 const { mongo } = require("../helper");
 const shell = require("shelljs");
-const fs = require("fs");
-const probe = require("probe-image-size");
 
-async function generateThumb(url, author, permlink) {
+const queue = new PQueue({ concurrency: 1 });
+
+async function getVideoSize(url) {
   return new Promise((resolve, reject) => {
     const command = `ffprobe -i  ${url} -show_entries stream=width,height -of json=compact=1 -v error`;
     shell.exec(command, { async: false }, (code, stdout, stderr) => {
       const result = JSON.parse(stdout.toString());
-      console.log(JSON.stringify(result));
+      if (result.streams === undefined) {
+        throw new Error(`Didn't find data for video ${url}`);
+      }
       return resolve({
         width: result.streams[0].width,
         height: result.streams[0].height,
       });
     });
   });
+}
+
+function getVideoPlayUrl(video) {
+  let playUrl;
+  if (video.upload_type === "ipfs") {
+    playUrl = `${APP_BUNNY_IPFS_CDN}/ipfs/${video.video_v2.replace(
+      "ipfs://",
+      ""
+    )}`;
+  } else {
+    playUrl = `${APP_VIDEO_CDN_DOMAIN}/${video.permlink}/default.m3u8`;
+  }
+  playUrl = playUrl.replace("manifest.m3u8", "480p/index.m3u8");
+  return playUrl;
+}
+
+async function updateVideoSizeTask(video) {
+  const startDate = Date.now();
+  const playUrl = getVideoPlayUrl(video);
+  try {
+    const result = await getVideoSize(playUrl);
+    video.width = result.width;
+    video.height = result.height;
+    await video.save();
+    const endDate = Date.now();
+    const date_diff = Math.abs(startDate - endDate) / 1000.0;
+    console.log(
+      `@${video.owner}/${video.permlink}, width: ${result.width}, height: ${result.height},  Time: ${date_diff} seconds`
+    );
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 (async () => {
@@ -25,35 +60,11 @@ async function generateThumb(url, author, permlink) {
     width: { $eq: null },
     height: { $eq: null },
   })
-    .limit(1)
+    .limit(20)
     .sort("-created");
 
   for (const video of videos) {
-    console.log(`================================`);
-    console.log(
-      `Started for @${video.owner}/${video.permlink} - ${Date.now().toString()}`
-    );
-    let playUrl;
-    if (video.upload_type === "ipfs") {
-      playUrl = `${APP_BUNNY_IPFS_CDN}/ipfs/${video.video_v2.replace(
-        "ipfs://",
-        ""
-      )}`;
-    } else {
-      playUrl = `${APP_VIDEO_CDN_DOMAIN}/${video.permlink}/default.m3u8`;
-    }
-    const result = await generateThumb(
-      playUrl.replace("manifest.m3u8", "480p/index.m3u8"),
-      video.owner,
-      video.permlink
-    );
-    video.width = result.width;
-    video.height = result.height;
-    await video.save();
-    console.log(
-      `Ended for @${video.owner}/${video.permlink} - ${Date.now().toString()}`
-    );
+    await updateVideoSizeTask(video);
   }
-
   process.exit(0);
 })();
